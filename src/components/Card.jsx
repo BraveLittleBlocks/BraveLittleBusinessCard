7a96a8453f02c1a6e0aa2fe47d116e2eee8472ec
Carlos <bravelittleblock@gmail.com>
2023-06-07 15:21:19 -0600

    added font styles to list items

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ function Card(){
                     <img src='src\assets\Bear_mid_profile_pic.PNG' className="card-img"/>
                     <p className="card-p">Brave Little Blocks: Web3 Developer</p>
                 </div>
-                <ul>
+                <ul className="list-styles">
                         <li>React and Solidity Focused</li>
                         <li>Github: BraveLittleBlocks</li>
                         <li>Twitter: @BraveBlock</li>
