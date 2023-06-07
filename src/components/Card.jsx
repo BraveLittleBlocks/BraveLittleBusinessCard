cd53d779d89c878caa7d1d9792b87651e8fe5787
Carlos <bravelittleblock@gmail.com>
2023-06-07 15:00:40 -0600

    added styles and general outline

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+
+
+function Card(){
+    return(
+        <div className="container">
+            <div className='card'>
+                <div className="image-text-wrapper">
+                    <img src='src\assets\Bear_mid_profile_pic.PNG' className="card-img"/>
+                    <p className="card-p">Brave Little Blocks: Web3 Developer</p>
+                </div>
+                <ul>
+                        <li>React and Solidity Focused</li>
+                        <li>Github: BraveLittleBlocks</li>
+                        <li>Twitter: @BraveBlock</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Card
